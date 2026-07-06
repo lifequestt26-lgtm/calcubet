357b2611914923c7d53facd8925238e19d0fa864
lifequestt26-lgtm <lifequestt26@gmail.com>
2026-07-06 10:34:13 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'trade-gol-v1';
+const assets = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
