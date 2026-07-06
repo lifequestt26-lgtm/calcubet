542a735180c9643d9ae755e6de4e28d32ab0f469
lifequestt26-lgtm <lifequestt26@gmail.com>
2026-07-06 12:46:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,26 @@
-const CACHE_NAME = 'trade-gol-v1';
-const assets = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'calc-trade-v2';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+  // Ignora o cache para o arquivo de configuração para que a mudança de senha seja instantânea
+  if (e.request.url.includes('config.json')) {
+    return e.respondWith(fetch(e.request));
+  }
+  
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
